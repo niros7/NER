79f85ef5b7086c426f215010361f879998d40693
Unknown <nirhaviv@gmail.com>
2018-03-10 20:25:52 +0200

    tutorial

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dframe = pd.read_csv("ner_dataset.csv", encoding="ISO-8859-1", error_bad_lines=False)
-
-df = dframe[:5000]
-x = df['Sentence #'].dropna().index
-print(df[24:53][['Word', 'Tag']])
-print('walla')
