03b23da3e69bfbc94c13b1fb5aa2b70b4ed790c7
Unknown <nirhaviv@gmail.com>
2018-03-10 16:53:51 +0200

    train on dataframe without prepare

--- model.py
+++ model.py
@@ -13,59 +13,59 @@ LEARNING_RATE = 0.01
 WEIGHT_DECAY = 1e-4
 SAVE_EVERY = 10
 
-PAD = "<PAD>"  # padding
-EOS = "<EOS>"  # end of sequence
-SOS = "<SOS>"  # start of sequence
+PAD = "<PAD>" # padding
+EOS = "<EOS>" # end of sequence
+SOS = "<SOS>" # start of sequence
 
 PAD_IDX = 0
 EOS_IDX = 1
 SOS_IDX = 2
 
 torch.manual_seed(1)
-CUDA = torch.cuda.is_available()
-
+CUDA = False
 
 class lstm_crf(nn.Module):
+
     def __init__(self, vocab_size, tag_to_idx):
         super(lstm_crf, self).__init__()
         self.tag_to_idx = tag_to_idx
         self.tagset_size = len(tag_to_idx)
-        self.lengths = []  # sequence lengths
+        self.lengths = [] # sequence lengths
 
         # architecture
-        self.embed = nn.Embedding(vocab_size, EMBED_SIZE, padding_idx=PAD_IDX)
+        self.embed = nn.Embedding(vocab_size, EMBED_SIZE, padding_idx = PAD_IDX)
         self.lstm = nn.LSTM(
-            input_size=EMBED_SIZE,
-            hidden_size=HIDDEN_SIZE // NUM_DIRS,
-            num_layers=NUM_LAYERS,
-            bias=True,
-            batch_first=True,
-            dropout=DROPOUT,
-            bidirectional=BIDIRECTIONAL
+            input_size = EMBED_SIZE,
+            hidden_size = HIDDEN_SIZE // NUM_DIRS,
+            num_layers = NUM_LAYERS,
+            bias = True,
+            batch_first = True,
+            dropout = DROPOUT,
+            bidirectional = BIDIRECTIONAL
         )
-        self.out = nn.Linear(HIDDEN_SIZE, self.tagset_size)  # LSTM output to tag
+        self.out = nn.Linear(HIDDEN_SIZE, self.tagset_size) # LSTM output to tag
 
         # matrix of transition scores from j to i
         self.trans = nn.Parameter(randn(self.tagset_size, self.tagset_size))
-        self.trans.data[SOS_IDX, :] = -10000.  # no transition to SOS
-        self.trans.data[:, EOS_IDX] = -10000.  # no transition from EOS except to PAD
-        self.trans.data[:, PAD_IDX] = -10000.  # no transition from PAD except to PAD
-        self.trans.data[PAD_IDX, :] = -10000.  # no transition to PAD except from EOS
+        self.trans.data[SOS_IDX, :] = -10000. # no transition to SOS
+        self.trans.data[:, EOS_IDX] = -10000. # no transition from EOS except to PAD
+        self.trans.data[:, PAD_IDX] = -10000. # no transition from PAD except to PAD
+        self.trans.data[PAD_IDX, :] = -10000. # no transition to PAD except from EOS
         self.trans.data[PAD_IDX, EOS_IDX] = 0.
         self.trans.data[PAD_IDX, PAD_IDX] = 0.
 
-    def init_hidden(self):  # initialize hidden states
-        h = Var(zeros(NUM_LAYERS * NUM_DIRS, BATCH_SIZE, HIDDEN_SIZE // NUM_DIRS))  # hidden states
-        c = Var(zeros(NUM_LAYERS * NUM_DIRS, BATCH_SIZE, HIDDEN_SIZE // NUM_DIRS))  # cell states
+    def init_hidden(self): # initialize hidden states
+        h = Var(zeros(NUM_LAYERS * NUM_DIRS, BATCH_SIZE, HIDDEN_SIZE // NUM_DIRS)) # hidden states
+        c = Var(zeros(NUM_LAYERS * NUM_DIRS, BATCH_SIZE, HIDDEN_SIZE // NUM_DIRS)) # cell states
         return (h, c)
 
-    def lstm_forward(self, x):  # LSTM forward pass
+    def lstm_forward(self, x): # LSTM forward pass
         self.hidden = self.init_hidden()
         self.lengths = [len_unpadded(seq) for seq in x]
         embed = self.embed(x)
-        embed = nn.utils.rnn.pack_padded_sequence(embed, self.lengths, batch_first=True)
+        embed = nn.utils.rnn.pack_padded_sequence(embed, self.lengths, batch_first = True)
         y, _ = self.lstm(embed, self.hidden)
-        y, _ = nn.utils.rnn.pad_packed_sequence(y, batch_first=True)
+        y, _ = nn.utils.rnn.pad_packed_sequence(y, batch_first = True)
         # y = y.contiguous().view(-1, HIDDEN_SIZE)
         y = self.out(y)
         # y = y.view(BATCH_SIZE, -1, self.tagset_size)
@@ -75,7 +75,7 @@ class lstm_crf(nn.Module):
         score = Var(Tensor(BATCH_SIZE).fill_(0.))
         y0 = torch.cat([LongTensor(BATCH_SIZE, 1).fill_(SOS_IDX), y0], 1)
         for b in range(len(self.lengths)):
-            for t in range(self.lengths[b]):  # iterate through the sequence
+            for t in range(self.lengths[b]): # iterate through the sequence
                 emit_score = y[b, t, y0[b, t + 1]]
                 trans_score = self.trans[y0[b, t + 1], y0[b, t]]
                 score[b] = score[b] + emit_score + trans_score
@@ -84,45 +84,45 @@ class lstm_crf(nn.Module):
     def crf_score_batch(self, y, y0, mask):
         score = Var(Tensor(BATCH_SIZE).fill_(0.))
         y0 = torch.cat([LongTensor(BATCH_SIZE, 1).fill_(SOS_IDX), y0], 1)
-        for t in range(y.size(1)):  # iterate through the sequence
+        for t in range(y.size(1)): # iterate through the sequence
             mask_t = Var(mask[:, t])
             emit_score = torch.cat([y[b, t, y0[b, t + 1]] for b in range(BATCH_SIZE)])
             trans_score = torch.cat([self.trans[seq[t + 1], seq[t]] for seq in y0]) * mask_t
             score = score + emit_score + trans_score
         return score
 
-    def crf_forward(self, y):  # forward algorithm for CRF
+    def crf_forward(self, y): # forward algorithm for CRF
         # initialize forward variables in log space
         score = Tensor(BATCH_SIZE, self.tagset_size).fill_(-10000.)
         score[:, SOS_IDX] = 0.
         score = Var(score)
         for b in range(len(self.lengths)):
-            for t in range(self.lengths[b]):  # iterate through the sequence
-                score_t = []  # forward variables at this timestep
-                for f in range(self.tagset_size):  # for each next tag
+            for t in range(self.lengths[b]): # iterate through the sequence
+                score_t = [] # forward variables at this timestep
+                for f in range(self.tagset_size): # for each next tag
                     emit_score = y[b, t, f].expand(self.tagset_size)
                     trans_score = self.trans[f].expand(self.tagset_size)
                     z = log_sum_exp(score[b] + emit_score + trans_score)
                     score_t.append(z)
                 score[b] = torch.cat(score_t)
-        score = torch.cat([log_sum_exp(i) for i in score])  # partition function
+        score = torch.cat([log_sum_exp(i) for i in score]) # partition function
         return score
 
-    def crf_forward_batch(self, y, mask):  # forward algorithm for CRF
+    def crf_forward_batch(self, y, mask): # forward algorithm for CRF
         # initialize forward variables in log space
         score = Tensor(BATCH_SIZE, self.tagset_size).fill_(-10000.)
         score[:, SOS_IDX] = 0.
         score = Var(score)
-        for t in range(y.size(1)):  # iterate through the sequence
-            score_t = []  # forward variables at this timestep
-            len_t = int(torch.sum(mask[:, t]))  # masked batch length
-            for f in range(self.tagset_size):  # for each next tag
+        for t in range(y.size(1)): # iterate through the sequence
+            score_t = [] # forward variables at this timestep
+            len_t = int(torch.sum(mask[:, t])) # masked batch length
+            for f in range(self.tagset_size): # for each next tag
                 emit_score = torch.cat([y[b, t, f].expand(1, self.tagset_size) for b in range(len_t)])
                 trans_score = self.trans[f].expand(len_t, self.tagset_size)
                 z = log_sum_exp_batch2(score, emit_score + trans_score)
                 score_t.append(z)
             score = torch.cat(score_t, 1)
-        score = log_sum_exp_batch1(score).view(BATCH_SIZE)  # partition function
+        score = log_sum_exp_batch1(score).view(BATCH_SIZE) # partition function
         return score
 
     def viterbi(self, y):
@@ -132,13 +132,13 @@ class lstm_crf(nn.Module):
         score[SOS_IDX] = 0.
         score = Var(score)
 
-        for t in range(len(y)):  # iterate through the sequence
+        for t in range(len(y)): # iterate through the sequence
             # backpointers and viterbi variables at this timestep
             bptr_t = []
             score_t = []
-            for i in range(self.tagset_size):  # for each next tag
+            for i in range(self.tagset_size): # for each next tag
                 z = score + self.trans[i]
-                best_tag = argmax(z)  # find the best previous tag
+                best_tag = argmax(z) # find the best previous tag
                 bptr_t.append(best_tag)
                 score_t.append(z[best_tag])
             bptr.append(bptr_t)
@@ -166,10 +166,10 @@ class lstm_crf(nn.Module):
         y = y * Var(mask.unsqueeze(-1).expand_as(y))
         score = self.crf_score_batch(y, y0, mask)
         Z = self.crf_forward_batch(y, mask)
-        L = torch.mean(Z - score)  # negative log likelihood
+        L = torch.mean(Z - score) # negative log likelihood
         return L
 
-    def forward(self, x):  # LSTM-CRF forward for prediction
+    def forward(self, x): # LSTM-CRF forward for prediction
         result = []
         y = self.lstm_forward(x)
         for i in range(len(self.lengths)):
@@ -180,53 +180,43 @@ class lstm_crf(nn.Module):
             result.append(best_path)
         return result
 
-
 def Tensor(*args):
     x = torch.Tensor(*args)
     return x.cuda() if CUDA else x
-
 
 def LongTensor(*args):
     x = torch.LongTensor(*args)
     return x.cuda() if CUDA else x
 
-
 def randn(*args):
     x = torch.randn(*args)
     return x.cuda() if CUDA else x
-
 
 def zeros(*args):
     x = torch.zeros(*args)
     return x.cuda() if CUDA else x
 
-
-def len_unpadded(x):  # get unpadded sequence length
+def len_unpadded(x): # get unpadded sequence length
     return next((i for i, j in enumerate(x) if scalar(j) == 0), len(x))
-
 
 def scalar(x):
     return x.view(-1).data.tolist()[0]
 
-
 def argmax(x):
-    return scalar(torch.max(x, 0)[1])  # for 1D tensor
-
+    return scalar(torch.max(x, 0)[1]) # for 1D tensor
 
 def log_sum_exp(x):
     max_score = x[argmax(x)]
     max_score_broadcast = max_score.expand_as(x)
     return max_score + torch.log(torch.sum(torch.exp(x - max_score_broadcast)))
 
-
-def log_sum_exp_batch1(x):  # for a single vector
+def log_sum_exp_batch1(x): # for a single vector
     max_score = torch.cat([i[argmax(i)] for i in x])
     max_score_broadcast = max_score.view(-1, 1).expand_as(x)
     z = max_score + torch.log(torch.sum(torch.exp(x - max_score_broadcast), 1))
     return z
 
-
-def log_sum_exp_batch2(x, y):  # for two vectors of different sizes
+def log_sum_exp_batch2(x, y): # for two vectors of different sizes
     z = x[:len(y)] + y
     max_score = torch.cat([i[argmax(i)] for i in z])
     max_score_broadcast = max_score.view(-1, 1).expand_as(z)
--- train.py
+++ train.py
@@ -3,52 +3,75 @@ import re
 from model import *
 from utils import *
 from os.path import isfile
+import pandas as pd
+
 
 def load_data():
+    dframe = pd.read_csv("ner_dataset.csv", encoding="ISO-8859-1", error_bad_lines=False)
+
+    df = dframe[:5000]
+    start_of_sentences_idx = df['Sentence #'].dropna().index
+
+    training_data = []
+    all_tags = df['Tag'].unique().ravel()
+
+    for curr_idx in range(len(start_of_sentences_idx) - 1):
+        curr_sentence_idx = start_of_sentences_idx[curr_idx]
+        next_sentence_idx = start_of_sentences_idx[curr_idx + 1]
+        sentence = df[['Word']][curr_sentence_idx:next_sentence_idx - 1].values.ravel()
+        tags = df[['Tag']][curr_sentence_idx:next_sentence_idx - 1].values.ravel()
+        training_data.append((sentence, tags))
+
+    training_data.sort(key=lambda tup: len(tup[0]), reverse=True)
+    word_to_idx = {}
+    for sentence, tags in training_data:
+        for word in sentence:
+            if word not in word_to_idx:
+                word_to_idx[word] = len(word_to_idx)
+
+    tag_to_idx = {PAD: 0, EOS: 1, SOS: 2}
+    for tag in all_tags:
+        if tag not in tag_to_idx:
+            tag_to_idx[tag] = len(tag_to_idx)
+
     data = []
     batch_x = []
     batch_y = []
-    batch_len = 0 # maximum sequence length of a mini-batch
-    print("loading data...")
-    word_to_idx = load_word_to_idx(sys.argv[2])
-    tag_to_idx = load_tag_to_idx(sys.argv[3])
-    fo = open(sys.argv[4], "r")
-    for line in fo:
-        line = line.strip()
-        words = [int(i) for i in line.split(" ")]
-        seq_len = words.pop()
-        if len(batch_x) == 0: # the first line has the maximum sequence length
-            batch_len = seq_len
+    batch_len = 40  # maximum sequence length of a mini-batch
+    for line in training_data:
+        sentence, tags = line
+        sentence, tags = [word_to_idx[w] for w in sentence], [tag_to_idx[t] for t in tags]
+        seq_len = len(sentence)
         pad = [PAD_IDX] * (batch_len - seq_len)
-        batch_x.append(words[:seq_len] + [EOS_IDX] + pad)
-        batch_y.append(words[seq_len:] + [EOS_IDX] + pad)
+        batch_x.append(sentence + [EOS_IDX] + pad)
+        batch_y.append(tags + [EOS_IDX] + pad)
         if len(batch_x) == BATCH_SIZE:
-            data.append((Var(LongTensor(batch_x)), LongTensor(batch_y))) # append a mini-batch
+            data.append((Var(LongTensor(batch_x)), LongTensor(batch_y)))  # append a mini-batch
             batch_x = []
             batch_y = []
-    fo.close()
     print("data size: %d" % (len(data) * BATCH_SIZE))
     print("batch size: %d" % BATCH_SIZE)
     return data, word_to_idx, tag_to_idx
 
+
 def train():
-    num_epochs = int(sys.argv[5])
+    num_epochs = 10
     data, word_to_idx, tag_to_idx = load_data()
     model = lstm_crf(len(word_to_idx), tag_to_idx)
-    optim = torch.optim.SGD(model.parameters(), lr = LEARNING_RATE, weight_decay = WEIGHT_DECAY)
-    epoch = load_checkpoint(sys.argv[1], model) if isfile(sys.argv[1]) else 0
+    optim = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+    epoch = 0
     if CUDA:
         model = model.cuda()
-    filename = re.sub("\.epoch[0-9]+$", "", sys.argv[1])
+    filename = "walla"
     print(model)
     print("training model...")
     for i in range(epoch + 1, epoch + num_epochs + 1):
         loss_sum = 0
         for x, y in data:
             model.zero_grad()
-            loss = model.loss(x, y) # forward pass and compute loss
-            loss.backward() # compute gradients
-            optim.step() # update parameters
+            loss = model.loss(x, y)  # forward pass and compute loss
+            loss.backward()  # compute gradients
+            optim.step()  # update parameters
             loss = scalar(loss)
             loss_sum += loss
         if i % SAVE_EVERY and i != epoch + num_epochs:
@@ -56,8 +79,7 @@ def train():
         else:
             save_checkpoint(filename, model, i, loss_sum / len(data))
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 6:
-        sys.exit("Usage: %s model word_to_idx tag_to_idx training_data num_epoch" % sys.argv[0])
     print("cuda: %s" % CUDA)
     train()
